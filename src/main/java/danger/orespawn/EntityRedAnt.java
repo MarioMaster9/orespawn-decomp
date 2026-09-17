614cac1a63534e71cdc20a6eea5b1c65ecbf3fa9
riall <71668864+break-core@users.noreply.github.com>
2026-09-17 04:05:41 -0400

    100% match EntityRedAnt

--- src/main/java/danger/orespawn/EntityRedAnt.java
+++ src/main/java/danger/orespawn/EntityRedAnt.java
@@ -14,11 +14,27 @@ import net.minecraft.util.DamageSource;
 import net.minecraft.world.EnumDifficulty;
 import net.minecraft.world.World;
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 public class EntityRedAnt extends EntityAnt {
 	int attack_delay = 20;
 
+
 	public EntityRedAnt(World par1World) {
 		super(par1World);
+
 		this.setSize(0.2F, 0.2F);
 		this.moveSpeed = (double)0.2F;
 		this.experienceValue = 1;
@@ -26,11 +42,9 @@ public class EntityRedAnt extends EntityAnt {
 		this.tasks.addTask(0, new EntityAIPanic(this, (double)1.4F));
 		this.tasks.addTask(1, new EntityAIAttackOnCollide(this, EntityPlayer.class, 1.0D, false));
 		this.tasks.addTask(2, new MyEntityAIWanderALot(this, 10, 1.0D));
-		if (OreSpawnMain.PlayNicely == 0) {
-			this.targetTasks.addTask(1, new EntityAINearestAttackableTarget(this, EntityPlayer.class, 4, true));
-		}
-
+		if (OreSpawnMain.PlayNicely == 0) this.targetTasks.addTask(1, new EntityAINearestAttackableTarget(this, EntityPlayer.class, 4, true));
 	}
+
 
 	protected void applyEntityAttributes() {
 		super.applyEntityAttributes();
@@ -39,66 +53,81 @@ public class EntityRedAnt extends EntityAnt {
 		this.getEntityAttribute(SharedMonsterAttributes.attackDamage).setBaseValue(1.0D);
 	}
 
+
+
 	public int mygetMaxHealth() {
 		return 2;
 	}
 
+
+
+
 	public boolean attackEntityAsMob(Entity par1Entity) {
-		if (OreSpawnMain.OreSpawnRand.nextInt(15) != 0) {
-			return false;
-		} else if (this.worldObj.difficultySetting == EnumDifficulty.PEACEFUL) {
-			return false;
-		} else {
-			boolean var4 = par1Entity.attackEntityFrom(DamageSource.causeMobDamage(this), 1.0F);
-			return var4;
-		}
+		if (OreSpawnMain.OreSpawnRand.nextInt(15) != 0) return false;
+		if (this.worldObj.difficultySetting == EnumDifficulty.PEACEFUL) return false;
+		boolean var4 = par1Entity.attackEntityFrom(DamageSource.causeMobDamage(this), 1.0F);
+		return var4;
 	}
 
+
+
+
+
+
+
 	public boolean interact(EntityPlayer par1EntityPlayer) {
-		if (par1EntityPlayer == null) {
-			return false;
-		} else if (!(par1EntityPlayer instanceof EntityPlayerMP)) {
-			return false;
-		} else {
-			ItemStack var2 = par1EntityPlayer.inventory.getCurrentItem();
-			if (var2 != null && var2.stackSize <= 0) {
+		if (par1EntityPlayer == null) return false;
+
+
+
+
+
+		if (!(par1EntityPlayer instanceof EntityPlayerMP)) return false;
+
+
+		ItemStack var2 = par1EntityPlayer.inventory.getCurrentItem();
+		if (var2 != null) {
+			if (var2.stackSize <= 0) {
 				par1EntityPlayer.inventory.setInventorySlotContents(par1EntityPlayer.inventory.currentItem, (ItemStack)null);
 				var2 = null;
 			}
-
-			if (var2 != null) {
-				return false;
-			} else {
-				if (par1EntityPlayer.dimension != OreSpawnMain.DimensionID2) {
-					MinecraftServer.getServer().getConfigurationManager().transferPlayerToDimension((EntityPlayerMP)par1EntityPlayer, OreSpawnMain.DimensionID2, new OreSpawnTeleporter(MinecraftServer.getServer().worldServerForDimension(OreSpawnMain.DimensionID2), OreSpawnMain.DimensionID2, this.worldObj));
-				} else {
-					MinecraftServer.getServer().getConfigurationManager().transferPlayerToDimension((EntityPlayerMP)par1EntityPlayer, 0, new OreSpawnTeleporter(MinecraftServer.getServer().worldServerForDimension(0), 0, this.worldObj));
-				}
-
-				return true;
-			}
 		}
+		if (var2 != null) {
+			return false;
+		}
+
+		if (par1EntityPlayer.dimension != OreSpawnMain.DimensionID2) {
+			MinecraftServer.getServer().getConfigurationManager().transferPlayerToDimension((EntityPlayerMP)par1EntityPlayer, OreSpawnMain.DimensionID2, new OreSpawnTeleporter(MinecraftServer.getServer().worldServerForDimension(OreSpawnMain.DimensionID2), OreSpawnMain.DimensionID2, this.worldObj));
+		} 
+		else {
+			MinecraftServer.getServer().getConfigurationManager().transferPlayerToDimension((EntityPlayerMP)par1EntityPlayer, 0, new OreSpawnTeleporter(MinecraftServer.getServer().worldServerForDimension(0), 0, this.worldObj));
+		}
+
+		
+		return true;
 	}
+
+
+
+
+
+
+
+
+
+
 
 	public void onUpdate() {
 		super.onUpdate();
-		if (!this.isDead) {
-			if (this.attack_delay > 0) {
-				--this.attack_delay;
-			}
-
-			if (this.attack_delay <= 0) {
-				this.attack_delay = 20;
-				if (this.worldObj.difficultySetting != EnumDifficulty.PEACEFUL) {
-					if (OreSpawnMain.PlayNicely == 0) {
-						EntityLivingBase e = this.worldObj.getClosestVulnerablePlayerToEntity(this, 1.5D);
-						if (e != null) {
-							this.attackEntityAsMob(e);
-						}
-
-					}
-				}
-			}
+		if (this.isDead) return;
+		if (this.attack_delay > 0) --this.attack_delay;
+		if (this.attack_delay > 0) return;
+		this.attack_delay = 20;
+		if (this.worldObj.difficultySetting == EnumDifficulty.PEACEFUL) return;
+		if (OreSpawnMain.PlayNicely != 0) return;
+		EntityLivingBase e = this.worldObj.getClosestVulnerablePlayerToEntity(this, 1.5D);
+		if (e != null) {
+			this.attackEntityAsMob(e);
 		}
 	}
 }
