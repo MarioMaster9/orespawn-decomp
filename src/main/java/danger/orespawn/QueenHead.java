c4b5a652d484e7cddbe4a3f1a64b24983ec8689f
MarioMaster9 <73719966+MarioMaster9@users.noreply.github.com>
2026-09-20 20:39:49 -0400

    match a few files
    KingHead.java
    KingSpawnerBlock.java
    QueenHead.java
    QueenSpawnerBlock.java
    ThePrince.java
    ThePrinceAdult.java

--- src/main/java/danger/orespawn/QueenHead.java
+++ src/main/java/danger/orespawn/QueenHead.java
@@ -11,7 +11,49 @@ import net.minecraft.util.DamageSource;
 import net.minecraft.util.MathHelper;
 import net.minecraft.world.World;
 
-public class QueenHead extends EntityLiving {
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+public class QueenHead extends EntityLiving
+{
 	private int boatPosRotationIncrements;
 	private double boatX;
 	private double boatY;
@@ -22,7 +64,8 @@ public class QueenHead extends EntityLiving {
 	private double velocityY;
 	private double velocityZ;
 
-	public QueenHead(World par1World) {
+	public QueenHead(World par1World)
+	{
 		super(par1World);
 		this.setSize(19.9F, 10.0F);
 		this.noClip = true;
@@ -30,7 +73,9 @@ public class QueenHead extends EntityLiving {
 		this.isImmuneToFire = true;
 	}
 
-	protected void applyEntityAttributes() {
+	
+	protected void applyEntityAttributes()
+	{
 		super.applyEntityAttributes();
 		this.getEntityAttribute(SharedMonsterAttributes.maxHealth).setBaseValue((double)OreSpawnMain.TheQueen_stats.health);
 		this.getEntityAttribute(SharedMonsterAttributes.movementSpeed).setBaseValue((double)1.33F);
@@ -42,62 +87,88 @@ public class QueenHead extends EntityLiving {
 		return false;
 	}
 
-	protected void fall(float par1) {
-	}
+	
+	
+	protected void fall(float par1) {}
 
-	protected void updateFallState(double par1, boolean par3) {
-	}
+	protected void updateFallState(double par1, boolean par3) {}
 
-	protected boolean canTriggerWalQueen() {
+	
+	
+	
+	// CTRL+F replace mistake
+	protected boolean canTriggerWalQueen()
+	{
 		return false;
 	}
 
-	protected void entityInit() {
+	protected void entityInit()
+	{
 		super.entityInit();
 	}
 
-	public boolean canBePushed() {
+	
+	/**
+	 * Returns true if this entity should push and be pushed by other entities when colliding.
+	 */
+	public boolean canBePushed()
+	{
 		return true;
 	}
 
-	public boolean attackEntityFrom(DamageSource par1DamageSource, float par2) {
+	
+	
+	
+	
+	public boolean attackEntityFrom(DamageSource par1DamageSource, float par2)
+	{
 		boolean ret = false;
-		if (par1DamageSource.getDamageType().equals("inWall")) {
+		
+		if (par1DamageSource.getDamageType().equals("inWall"))
 			return false;
-		} else {
-			Entity e = par1DamageSource.getEntity();
-			if (e == null || !(e instanceof TheQueen) && !(e instanceof QueenHead)) {
-				e = par1DamageSource.getSourceOfDamage();
-				if (e == null || !(e instanceof TheQueen) && !(e instanceof QueenHead)) {
-					List var5 = this.worldObj.getEntitiesWithinAABB(TheQueen.class, this.boundingBox.expand((double)48.0F, 32.0D, (double)48.0F));
-					Iterator var2 = var5.iterator();
-					Entity var3 = null;
-					TheQueen var4 = null;
-					if (var2.hasNext()) {
-						var3 = (Entity)var2.next();
-						var4 = (TheQueen)var3;
-						ret = var4.attackEntityFrom(par1DamageSource, par2);
-					}
 
-					return ret;
-				} else {
-					return false;
-				}
-			} else {
-				return false;
-			}
+		Entity e = par1DamageSource.getEntity();
+		if (e != null && (e instanceof TheQueen || e instanceof QueenHead)) return false;
+		e = par1DamageSource.getSourceOfDamage();
+		if (e != null && (e instanceof TheQueen || e instanceof QueenHead)) return false;
+		
+		List var5 = this.worldObj.getEntitiesWithinAABB(TheQueen.class, this.boundingBox.expand((double)48.0F, 32.0D, (double)48.0F));
+		Iterator var2 = var5.iterator();
+		Entity var3 = null;
+		TheQueen var4 = null;
+		
+		if (var2.hasNext())
+		{
+			
+			var3 = (Entity)var2.next();
+			var4 = (TheQueen)var3;
+			ret = var4.attackEntityFrom(par1DamageSource, par2);
 		}
+		return ret;
 	}
 
-	public boolean canBeCollidedWith() {
+	/**
+	 * Returns true if other Entities should be prevented from moving through this Entity.
+     */
+	public boolean canBeCollidedWith()
+	{
 		return true;
 	}
 
+	
+	/**
+	 * Sets the position and rotation. Only difference from the other one is no bounding on the rotation. Args: posX,
+	 * posY, posZ, yaw, pitch
+	 */
 	@SideOnly(Side.CLIENT)
-	public void setPositionAndRotation2(double par1, double par3, double par5, float par7, float par8, int par9) {
-		if (this.riddenByEntity != null) {
+	public void setPositionAndRotation2(double par1, double par3, double par5, float par7, float par8, int par9)
+	{
+		if (this.riddenByEntity != null)
+		{
 			this.boatPosRotationIncrements = par9 + 8;
-		} else {
+		}
+		else
+		{
 			this.boatPosRotationIncrements = 6;
 		}
 
@@ -111,55 +182,83 @@ public class QueenHead extends EntityLiving {
 		this.motionZ = this.velocityZ;
 	}
 
+	
+	
+	/**
+	 * Sets the velocity to the args. Args: x, y, z
+	 */
 	@SideOnly(Side.CLIENT)
-	public void setVelocity(double par1, double par3, double par5) {
+	public void setVelocity(double par1, double par3, double par5)
+	{
 		this.velocityX = this.motionX = par1;
 		this.velocityY = this.motionY = par3;
 		this.velocityZ = this.motionZ = par5;
 	}
 
-	public void onUpdate() {
-		if (!this.isDead) {
-			this.isAirBorne = true;
-			this.setFire(0);
-			if (this.worldObj.isRemote) {
-				if (this.boatPosRotationIncrements > 0) {
-					double d4 = this.posX + (this.boatX - this.posX) / (double)this.boatPosRotationIncrements;
-					double d5 = this.posY + (this.boatY - this.posY) / (double)this.boatPosRotationIncrements;
-					double d11 = this.posZ + (this.boatZ - this.posZ) / (double)this.boatPosRotationIncrements;
-					this.setPosition(d4, d5, d11);
-					this.rotationPitch = (float)((double)this.rotationPitch + (this.boatPitch - (double)this.rotationPitch) / (double)this.boatPosRotationIncrements);
-					double d10 = MathHelper.wrapAngleTo180_double(this.boatYaw - (double)this.rotationYaw);
-					if (this.riddenByEntity != null) {
-						d10 = MathHelper.wrapAngleTo180_double((double)this.riddenByEntity.rotationYaw - (double)this.rotationYaw);
-					}
-
-					this.rotationYaw = (float)((double)this.rotationYaw + d10 / (double)this.boatPosRotationIncrements);
-					this.setRotation(this.rotationYaw, this.rotationPitch);
-					--this.boatPosRotationIncrements;
-				}
-			} else {
-				List var5 = this.worldObj.getEntitiesWithinAABB(TheQueen.class, this.boundingBox.expand(32.0D, 32.0D, 32.0D));
-				Iterator var2 = var5.iterator();
-				Entity var3 = null;
-				TheQueen var4 = null;
-				if (var2.hasNext()) {
-					var3 = (Entity)var2.next();
-					var4 = (TheQueen)var3;
-					this.posY = var4.posY + 12.0D;
-					this.posX = var4.posX - (double)30.0F * Math.sin(Math.toRadians((double)var4.rotationYawHead));
-					this.posZ = var4.posZ + (double)30.0F * Math.cos(Math.toRadians((double)var4.rotationYawHead));
-					this.rotationYaw = var4.rotationYaw;
-					this.rotationYawHead = var4.rotationYawHead;
-					this.motionX = var4.motionX;
-					this.motionY = var4.motionY;
-					this.motionZ = var4.motionZ;
-					this.setHealth(var4.getHealth());
-				} else {
-					this.setDead();
-				}
+	
+	
+	
+	/**
+	 * Called to update the entity's position/logic.
+	 */
+	public void onUpdate()
+	{
+		if (this.isDead) return;
+		this.isAirBorne = true;
+		this.setFire(0);
+		double d4;
+		double d5;
+		double d10;
+		double d11;
+		
+		if (this.worldObj.isRemote)
+		{
+			
+			if (this.boatPosRotationIncrements > 0)
+			{
+				d4 = this.posX + (this.boatX - this.posX) / (double)this.boatPosRotationIncrements;
+				d5 = this.posY + (this.boatY - this.posY) / (double)this.boatPosRotationIncrements;
+				d11 = this.posZ + (this.boatZ - this.posZ) / (double)this.boatPosRotationIncrements;
+				this.setPosition(d4, d5, d11);
+				
+				this.rotationPitch = (float)((double)this.rotationPitch + (this.boatPitch - (double)this.rotationPitch) / (double)this.boatPosRotationIncrements);
+				d10 = MathHelper.wrapAngleTo180_double(this.boatYaw - (double)this.rotationYaw);
+				if (this.riddenByEntity != null) d10 = MathHelper.wrapAngleTo180_double((double)this.riddenByEntity.rotationYaw - (double)this.rotationYaw);
+				this.rotationYaw = (float)((double)this.rotationYaw + d10 / (double)this.boatPosRotationIncrements);
+				this.setRotation(this.rotationYaw, this.rotationPitch);
+				
+				this.boatPosRotationIncrements--;
+				
+				
 			}
-
+			
+			
+			
+		}
+		else
+		{
+			
+			List var5 = this.worldObj.getEntitiesWithinAABB(TheQueen.class, this.boundingBox.expand(32.0D, 32.0D, 32.0D));
+			Iterator var2 = var5.iterator();
+			Entity var3 = null;
+			TheQueen var4 = null;
+			
+			if (var2.hasNext())
+			{
+				var3 = (Entity)var2.next();
+				var4 = (TheQueen)var3;
+				this.posY = var4.posY + 12.0D;
+				this.posX = var4.posX - (double)30.0F * Math.sin(Math.toRadians((double)var4.rotationYawHead));
+				this.posZ = var4.posZ + (double)30.0F * Math.cos(Math.toRadians((double)var4.rotationYawHead));
+				this.rotationYaw = var4.rotationYaw;
+				this.rotationYawHead = var4.rotationYawHead;
+				this.motionX = var4.motionX;
+				this.motionY = var4.motionY;
+				this.motionZ = var4.motionZ;
+				this.setHealth(var4.getHealth());
+			} else {
+				this.setDead();
+			}
 		}
 	}
 }
