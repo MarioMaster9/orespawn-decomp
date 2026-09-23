31ca05d0cb01eee609f40f427b6a7f2ca51a436f
Zero_DSRS_VX <24632178+PhoenixVX@users.noreply.github.com>
2026-09-23 17:27:18 -0600

    Use alternative variable for compiling

--- tools/setup.py
+++ tools/setup.py
@@ -2,16 +2,16 @@ from pathlib import Path
 
 import os
 
-def write_options(output_file, java_home):
+def write_options(output_file, javac):
 	try:
 		with open(output_file, "w", encoding="utf-8") as file:
-			file.write(f"org.gradle.java.home={java_home}\n")
+			file.write(f"java_compiler=\"{javac}\"\n")
 	except:
 		print(f"Unexpected exception when writing to {output_file}")
 
 def main():
 	javaHomeFound = False
-	javaHome = None
+	javac = None
 	while (not javaHomeFound):
 		javaHome = Path(input("Path to JDK 6: "))
 
@@ -28,14 +28,14 @@ def main():
 		else:
 			print(f"Could not find Java compiler from path: {javac}")
 
-	if javaHome is not None:
+	if javac is not None:
 		project_file = Path("build.gradle")
 
 		if not project_file.exists():
 			print("setup.py should be ran from the root of the repository!")
 		else:
 			gradle_properties = Path("gradle.properties")
-			write_options(gradle_properties, javaHome)
+			write_options(gradle_properties, javac)
 
 if __name__ == "__main__":
 	main()
